2ed7065bd248c848ecc1c5a5612886e44274956b
RedbeanW <redbeana44945@gmail.com>
2021-08-11 02:39:07 +0800

    rebuild.

--- bdxland.py
+++
@@ -1,8 +0,0 @@
-import leveldb
-
-db=leveldb.LevelDB('./input-data/bdxland')
-for key, value in db.RangeIter():
-    a=key.decode('unicode_escape')
-    b=value.decode('unicode_escape')
-    print(key,value)
-    print (a,b)
